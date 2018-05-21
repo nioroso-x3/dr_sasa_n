d75b34360a277c1c44190a2c0a7be2da8a577f9e
Judemir Ribeiro <nioroso_x3@hotmail.com>
2018-05-21 14:40:33 -0400

    Fix extension for windows.

--- src/compile_static.sh
+++ src/compile_static.sh
@@ -2,4 +2,4 @@
 #linux
 g++ -static -static-libgcc -static-libstdc++ -O3 -o dr_sasa -std=c++11 *.cpp
 #windows
-x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -O3 -o dr_sasa -std=c++11 *.cpp
+x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -O3 -o dr_sasa.exe -std=c++11 *.cpp
